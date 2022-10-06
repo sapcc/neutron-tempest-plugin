fb8404ed172d6d64a8ef5bfcb75ba04a813bfae8
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2022-10-06 22:33:02 +0300

    Delete sleeps

--- neutron_tempest_plugin/api/base.py
+++ neutron_tempest_plugin/api/base.py
@@ -108,7 +108,6 @@ class BaseNetworkTest(test.BaseTestCase):
 
     @classmethod
     def resource_setup(cls):
-        time.sleep(120)
         super(BaseNetworkTest, cls).resource_setup()
 
         cls.networks = []
@@ -292,7 +291,6 @@ class BaseNetworkTest(test.BaseTestCase):
                     network_segment_range['id'])
 
         super(BaseNetworkTest, cls).resource_cleanup()
-        time.sleep(120)
 
     @classmethod
     def _try_delete_resource(cls, delete_callable, *args, **kwargs):
